740fdb2d08cdeac7a0265faba409832afb0cb841
Isaac00rz <Isaac_marqz@outlook.com>
2018-11-21 11:28:47 -0600

    Scrip

--- Scrip.sql
+++ Scrip.sql
@@ -12,7 +12,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE CatalogoCuentas(
-noCuenta integer NOT NULL,
+noCuenta varchar(40) NOT NULL,
 nombre varchar(40) NOT NULL,
 tipo varchar(25) DEFAULT 'acredora',
 descripcion varchar(250),
@@ -26,14 +26,15 @@ id_Poliza int auto_increment,
 tipo varchar (15) NOT NULL,
 fecha date ,
 periodo int  NOT NULL,
-debe money,
-haber money,
+debe double,
+haber double,
 concepto varchar(200),
-noCuenta integer,
+noCuenta varchar(40) not null,
 PRIMARY KEY (id_Poliza),
+CONSTRAINT polizas_f
 FOREIGN KEY (noCuenta)
-REFERENCES SistemaContable.CatalogoCuentas(noCuenta)
-ON DELETE NO ACTION ON UPDATE NO ACTION
-)
+REFERENCES SistemaContable.CatalogoCuentas (noCuenta)
+ON DELETE NO ACTION 
+ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
